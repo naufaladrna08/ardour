fb47a0d644fdd903cac8ccc2c807579d137ee11a
naufaladrna08 <naufaladrn90@gmail.com>
2022-05-21 14:51:55 +0700

    added build env install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# These are build tools that you need to install
+# (compile) Ardour. Debian derived only since InOS
+# is Debian based.
+
+sudo apt install python2 install libbz2-dev libgtkmm-2.4-dev \
+ libcurl4-openssl-dev libasound-dev libsndfile1-dev libarchive-dev \
+ liblo-dev libtaglib-ocaml-dev librubberband-dev ibfftw3-dev \
+ libaubio-dev liblrdf0-dev lv2-dev libserd-dev libsord-dev \
+ libsratom-dev liblilv-dev 
